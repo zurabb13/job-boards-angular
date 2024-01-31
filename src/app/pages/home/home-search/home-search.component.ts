7d525945c0886d7a16e10db06d6df9d52b2df0d6
zurabb13 <zura.baramia98@outlook.com>
2024-01-31 15:40:12 +0400

    primeng

--- src/app/pages/home/home-search/home-search.component.ts
+++ src/app/pages/home/home-search/home-search.component.ts
@@ -1,10 +1,28 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { DropdownModule } from "primeng/dropdown";
 
+const MATERIAL = [DropdownModule];
 @Component({
   selector: "home-search",
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ...MATERIAL],
   templateUrl: "./home-search.component.html",
   styleUrl: "./home-search.component.scss",
 })
-export class HomeSearchComponent {}
+export class HomeSearchComponent {
+  cities = [
+    {
+      name: "saba",
+    },
+    {
+      name: "saba1",
+    },
+    {
+      name: "saba2",
+    },
+    {
+      name: "saba3",
+    },
+  ];
+}
